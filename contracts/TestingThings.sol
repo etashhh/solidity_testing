f62bd5c9c055fc780e000dc5400be44d933cd085
etash <erik.tastepe@gmail.com>
2022-05-27 16:08:44 -0400

    added prettier-solidity and reentrancy guard

--- contracts/TestingThings.sol
+++ contracts/TestingThings.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.8.0;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract TestingThings is ERC721A, Ownable {
-
+contract TestingThings is ERC721A, Ownable, ReentrancyGuard {
     uint256 public maxSupply;
     string private _tokenBaseURI;
 
     event SetTokenBaseURI(string indexed baseTokenURI);
 
-    constructor(string memory name, string memory symbol, uint256 _maxSupply) ERC721A(name, symbol) {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 _maxSupply
+    ) ERC721A(name, symbol) {
         maxSupply = _maxSupply;
     }
 
@@ -24,10 +28,16 @@ contract TestingThings is ERC721A, Ownable {
         return _tokenBaseURI;
     }
 
-    function mint(uint256 quantity) external payable {
+    function mint(uint256 quantity) external payable nonReentrant {
         uint256 price = 1000000000000000;
-        require(totalSupply() + quantity <= maxSupply, "You're trying to mint too many");
-        require(msg.value == price * quantity, "Did not send appropriate funds");
+        require(
+            totalSupply() + quantity <= maxSupply,
+            "You're trying to mint too many"
+        );
+        require(
+            msg.value == price * quantity,
+            "Did not send appropriate funds"
+        );
         _safeMint(msg.sender, quantity);
     }
 }
